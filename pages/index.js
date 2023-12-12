cced9df1b3fe60f9b56ee9a59a625235d39adf6b
Tej <tejmifysolutions@gmail.com>
2023-12-12 17:03:07 +0530

    first commit

--- pages/index.js
+++ pages/index.js
@@ -46,7 +46,7 @@ export default class Home extends React.Component {
             </div>
           </div>
         </div>
-        <DataFetchExample />
+        {/* <DataFetchExample /> */}
       </>
     );
   }
